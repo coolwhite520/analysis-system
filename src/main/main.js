fe5e65aa11ae5d18b6c4011f69024926c86cc73c
coolwhite520 <81929154@qq.com>
2020-07-06 15:53:50 +0800

    first modify

---
+++ src/main/main.js
@@ -0,0 +1,86 @@
+import { app, BrowserWindow } from "electron";
+
+/**
+ * Set `__static` path to static files in production
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
+ */
+if (process.env.NODE_ENV !== "development") {
+  global.__static = require("path")
+    .join(__dirname, "/static")
+    .replace(/\\/g, "\\\\");
+}
+
+let mainWindow;
+const winURL =
+  process.env.NODE_ENV === "development"
+    ? `http://localhost:9080`
+    : `file://${__dirname}/index.html`;
+
+function createWindow() {
+  /**
+   * Initial window options
+   */
+  mainWindow = new BrowserWindow({
+    height: 563,
+    useContentSize: true,
+    title: require("../../package.json").description,
+    width: 1000,
+    show: false,
+    // titleBarStyle: process.platform === "darwin" ? "hidden" : "default",
+    // backgroundColor: '#2e2c29', // 初始化一个背景色
+    webPreferences: {
+      nodeIntegration: true,
+      webSecurity: false,
+      enableRemoteModule: true,
+    },
+  });
+
+  mainWindow.loadURL(winURL);
+
+  mainWindow.maximize();
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show()
+  
+  })
+}
+
+
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+/**
+ * Auto Updater
+ *
+ * Uncomment the following code below and install `electron-updater` to
+ * support auto updating. Code Signing with a valid certificate is required.
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
+ */
+
+/*
+import { autoUpdater } from 'electron-updater'
+
+autoUpdater.on('update-downloaded', () => {
+  autoUpdater.quitAndInstall()
+})
+
+app.on('ready', () => {
+  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
+})
+ */
