936cfb302f741c1cc82a37239f6d69a675113ccb
coolwhite520 <81929154@qq.com>
2020-11-03 17:09:18 +0800

    asdf

--- src/renderer/pages/mini/DataCollection.vue
+++ src/renderer/pages/mini/DataCollection.vue
@@ -1131,6 +1131,7 @@ export default {
     );
     this.$electron.ipcRenderer.send("data-collection-open-complete");
     console.log(global.pool);
+    console.log(global.pool);
   },
   destroyed() {
     log.info("datacollection destroyed.");
