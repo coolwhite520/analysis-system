b7aa377fbe0b1c3ef001f5cec4528f6a9bdd299c
coolwhite520 <81929154@qq.com>
2020-11-10 09:41:52 +0800

    新增对exe和asar文件的头部加解密功能

--- vmpack/afterSign.js
+++ vmpack/afterSign.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const fs = require("fs");
 const shell = require("shelljs");
+const appName = require("../package.json").name;
 
 async function sleep(ms) {
   return new Promise((resolve, reject) => {
@@ -9,53 +10,68 @@ async function sleep(ms) {
     }, ms);
   });
 }
+// 加密analysis-system.exe 和 asar文件
+async function encrpyt(asarFilePath, exeFilePath) {
+  console.log("encrypt app.asar.... begin");
+  let encryptFilePath = path.join(__dirname, "encrypt.exe");
+  let cmd = `${encryptFilePath} ${asarFilePath} ${exeFilePath}`;
+  let batFileContent = `start /w ${cmd}`;
+  console.log(batFileContent);
+  let batFilepath = path.join(__dirname, "encrypt.bat");
+  fs.writeFileSync(batFilepath, batFileContent);
+  shelljs.exec(batFilepath, { silent: false, async: false });
+  fs.unlinkSync(batFilepath);
+  fs.unlinkSync(asarFilePath);
+  fs.unlinkSync(exeFilePath);
+  fs.renameSync(
+    path.join(path.dirname(asarFilePath), "app.out.asar"),
+    asarFilePath
+  );
+  fs.renameSync(
+    path.join(path.dirname(exeFilePath), `${appName}.out.exe`),
+    exeFilePath
+  );
+  fs.unlinkSync(path.join(path.dirname(asarFilePath), "app.out.asar"));
+  fs.unlinkSync(path.join(path.dirname(exeFilePath), `${appName}.out.exe`));
+  console.log("encrypt app.asar....end");
+}
+
+async function vmProtect(exeFilePath) {
+  // vmprotect保护
+  console.log("protect begin....");
+  let appOutDir = path.dirname(exeFilePath);
+  let newPathFileName = path.join(appOutDir, `${appName}.origin.exe`);
+  console.log("rename begin....");
+  fs.renameSync(exeFilePath, newPathFileName);
+  console.log("rename end....");
+  let batFilePath = path.join(__dirname, "protect.bat");
+  console.log(batFilePath);
+  shell.exec(batFilePath, { silent: false, async: false });
+  fs.unlinkSync(newPathFileName);
+  fs.unlinkSync(newPathFileName + ".log");
+  console.log("protect end....");
+}
 
 exports.default = async function(context) {
   console.log(context);
   let exeFilePath;
   switch (process.platform) {
     case "win32":
-      exeFilePath = path.join(context.appOutDir, "analysis-system.exe");
+      exeFilePath = path.join(context.appOutDir, `${appName}.exe`);
       break;
     case "darwin":
-      exeFilePath = path.join(context.appOutDir, "analysis-system.app");
+      exeFilePath = path.join(context.appOutDir, `${appName}.app`);
       break;
     case "linux":
-      exeFilePath = path.join(context.appOutDir, "analysis-system");
+      exeFilePath = path.join(context.appOutDir, `${appName}`);
       break;
   }
   if (process.platform === "win32" && fs.existsSync(exeFilePath)) {
-    // 新增asar修改而进程调用
-    console.log("wait for protect....");
-    console.log("wait for encrypt app.asar.... begin");
-    let encryptFilePath = path.join(__dirname, "encrypt.exe");
-    let asarFilePath = path.join(context.appOutDir, "\\resources\\app.asar");
-    let cmd = `${encryptFilePath} ${asarFilePath} ${exeFilePath}`;
-    console.log(cmd);
-    shell.exec(cmd, { silent: false, async: false });
-    console.log("wait for encrypt app.asar....end");
-    return;
-    // vmprotect保护
-    let newPathFileName = path.join(
-      context.appOutDir,
-      "analysis-system.origin.exe"
-    );
-    await sleep(5000);
     try {
-      console.log("rename begin....");
-      fs.renameSync(exeFilePath, newPathFileName);
-      console.log("rename end....");
-      await sleep(1000);
-      console.log("protect begin....");
-      let batFilePath = path.join(__dirname, "protect.bat");
-      console.log(batFilePath);
-      shell.exec(batFilePath, { silent: false, async: false });
-      console.log("protect end....");
-      await sleep(1000);
-      fs.unlinkSync(newPathFileName);
-      //analysis-system.origin.exe.log
-      fs.unlinkSync(newPathFileName + ".log");
-      await sleep(1000);
+      // 新增asar修改而进程调用
+      let asarFilePath = path.join(context.appOutDir, "\\resources\\app.asar");
+      await encrpyt(asarFilePath, exeFilePath);
+      await vmProtect(exeFilePath);
     } catch (e) {
       console.log(e.message);
     }
